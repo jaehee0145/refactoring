be61a41fa74db0825f808030c98dd1a93eb6e26c
Summer Park <summer@Summerui-MacBookPro.local>
2021-09-08 23:25:41 +0900

    Initial commit

---
+++ src/main/java/chap1/Customer.java
@@ -0,0 +1,68 @@
+package chap1;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+class Customer {
+    private String _name;
+    private Vector _rentals = new Vector();
+
+    public Customer(String _name) {
+        this._name = _name;
+    }
+
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration rentals = _rentals.elements();
+        String result = getName() + " 고객님의 대여 기록\n";
+        while (rentals.hasMoreElements()) {
+            double thisAmount = 0;
+            Rental each = (Rental) rentals.nextElement();
+
+            // 비디오 종류별 대여료 계산
+            switch (each.get_movie().get_priceCode()) {
+                case Movie.REGULAR:
+                    thisAmount += 2;
+                    if (each.get_daysRented() > 2) {
+                        thisAmount += (each.get_daysRented() -2) * 1.5;
+                    }
+                    break;
+                case Movie.NEW_RELEASE:
+                    thisAmount += each.get_daysRented() * 3;
+                    break;
+                case Movie.CHILDRENS:
+                    thisAmount += 1.5;
+                    if (each.get_daysRented() > 3) {
+                        thisAmount += (each.get_daysRented() - 3) * 1.5;
+                    }
+                    break;
+            }
+
+            // 적립 포인트를 1 포인트 증가
+            frequentRenterPoints ++;
+            // 최신물을 이틀 이상 대여하면 보너스 포인트 지급
+            if ((each.get_movie().get_priceCode() == Movie.NEW_RELEASE) &&
+            each.get_daysRented() > 1) {
+                frequentRenterPoints ++;
+            }
+
+            // 이번에 대여하는 비디오 정보와 대여료를 출력
+            result += "\t" + each.get_movie().get_title() + "\t" + String.valueOf(thisAmount) + "\n";
+            // 현재까지 누적된 총 대여료
+            totalAmount += thisAmount;
+        }
+        // 푸터 행 추가
+        result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
+        result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
+        return result;
+    }
+}
---
+++ src/main/java/chap1/Rental.java
@@ -0,0 +1,19 @@
+package chap1;
+
+class Rental {
+    private Movie _movie;
+    private int _daysRented;
+
+    public Rental(Movie _movie, int _daysRented) {
+        this._movie = _movie;
+        this._daysRented = _daysRented;
+    }
+
+    public Movie get_movie() {
+        return _movie;
+    }
+
+    public int get_daysRented() {
+        return _daysRented;
+    }
+}
