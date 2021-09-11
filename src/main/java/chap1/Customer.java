b4fc7132c896a533015abb0cdab2ebb318e13299
jaehee <jaeheepark0145@gmail.com>
2021-09-11 17:42:20 +0900

    #6 임시변수를 메서드 호출로 수정

--- src/main/java/chap1/Customer.java
+++ src/main/java/chap1/Customer.java
@@ -20,24 +20,38 @@ class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = getName() + " 고객님의 대여 기록\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
 
-            // 경우에 따른 적립 포인트 지급 함수를 호출
-            frequentRenterPoints = each.getFrequentRenterPoints();
-
             // 이번에 대여하는 비디오 정보와 대여료를 출력
             result += "\t" + each.get_movie().get_title() + "\t" + String.valueOf(each.getCharge()) + "\n";
             // 현재까지 누적된 총 대여료
-            totalAmount += each.getCharge();
         }
         // 푸터 행 추가
-        result += "누적 대여료: " + String.valueOf(totalAmount) + "\n";
-        result += "적립 포인트: " + String.valueOf(frequentRenterPoints);
+        result += "누적 대여료: " + String.valueOf(getTotalCharge()) + "\n";
+        result += "적립 포인트: " + String.valueOf(getTotalFrequentRenterPoints());
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
+        }
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();;
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 }
