51e57ffe62eadb002d9107e73ee24fb1337812ef
jaehee <jaeheepark0145@gmail.com>
2021-10-10 23:10:38 +0900

    Chapter 07 클래스 추출

---
+++ src/main/java/chap07/Person.java
@@ -0,0 +1,42 @@
+package chap07;
+
+public class Person {
+    public String getName() {
+        return name;
+    }
+
+    public String getTelephoneNumber() {
+        return officeTelephone.getTelephoneNumber();
+    }
+
+    TelephoneNumber getOfficeTelephone() {
+        return officeTelephone;
+    }
+
+    private String name;
+    private TelephoneNumber officeTelephone = new TelephoneNumber();
+
+    class TelephoneNumber {
+        public String getTelephoneNumber() {
+            return ("(" + areaCode + ") " + number);
+        }
+        public String getAreaCode() {
+            return areaCode;
+        }
+
+        public void setAreaCode(String areaCode) {
+            this.areaCode = areaCode;
+        }
+
+        public String getNumber() {
+            return number;
+        }
+
+        public void setNumber(String number) {
+            this.number = number;
+        }
+
+        private String number;
+        private String areaCode;
+    }
+}
